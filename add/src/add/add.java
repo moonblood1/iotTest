fd6a74aabb3a03a48642024b6b6e9de6b56c0f21
it <it@DESKTOP-KKAQ0RK>
2016-09-19 15:00:14 +0900

    commit

---
+++ add/src/add/add.java
@@ -0,0 +1,16 @@
+package add;
+
+public class add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a =1;
+		int b =2;
+		int c = a+b;
+		
+		System.out.println("답:"+c);
+
+	}
+
+}
